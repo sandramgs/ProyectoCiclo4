351aa9c4793b4a428cbd06efb7d5430a5257aea7
sandramgs <sandramgs7@gmail.com>
2021-12-15 12:43:16 -0500

    cambio para implementar el defaultValue en los campos del formulario para que presente los datos del registro que se quiere editar

--- src/componentes/formularios/FormInsumos.js
+++ src/componentes/formularios/FormInsumos.js
@@ -40,12 +40,10 @@ function FormInsumos({title = "", item = initialData}) {
         </Modal.Header>
         <Modal.Body>
           <Form>        
-            <input type="text" name="id" value={state.id} onChange={alCambiar}  />    
-            <input type="text" name="nombre" value={state.nombre} onChange={alCambiar}  />           
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridState" >
                 <Form.Label>Tipo de Insumo</Form.Label>
-                <Form.Select defaultValue="Choose..." name="tipoInsumo" disabled={title==="Editar"} onChange={alCambiar}>
+                <Form.Select defaultValue={state.tipoInsumo} name="tipoInsumo" disabled={title==="Editar"} onChange={alCambiar}>
                   <option selected>{state.tipoInsumo}</option>
                   <option>Semilla</option>
                   <option>Fertilizante</option>
@@ -55,7 +53,7 @@ function FormInsumos({title = "", item = initialData}) {
 
               <Form.Group as={Col} controlId="formGridState" >
                 <Form.Label>Unidad de Medida</Form.Label>
-                <Form.Select defaultValue="Choose..." name="unidad" disabled={title==="Editar"}>
+                <Form.Select defaultValue={state.unidad} name="unidad" disabled={title==="Editar"}>
                   <option selected onChange={alCambiar}>{state.unidad}</option>
                   <option>Kilos</option>
                   <option>Litros</option>
@@ -66,25 +64,23 @@ function FormInsumos({title = "", item = initialData}) {
 
             <Form.Group as={Col} controlId="formGridState" >
                 <Form.Label>Descripcion Insumo</Form.Label>
-                <Form.Control disabled={title==="Editar"} name="descripcion" value={state.descripcion} onChange={alCambiar}/>
+                <Form.Control disabled={title==="Editar"} name="descripcion" defaultValue={state.descripcion} onChange={alCambiar}/>
             </Form.Group>
 
             <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
               <Form.Label>Valor Unidad</Form.Label>
-              <Form.Control placeholder={state.valorUnidad} name="valorUnidad" onChange={alCambiar}/>
+              <Form.Control defaultValue={state.valorUnidad} name="valorUnidad" onChange={alCambiar}/>
             </Form.Group>
 
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridCity">
                 <Form.Label type="date">Tarifa Valida a partir de:</Form.Label>
-                <Form.Control placeholder={state.fechaInicioTarifa} name="fechaInicioTarifa" onChange={alCambiar}/>
+                <Form.Control defaultValue={state.fechaInicioTarifa} name="fechaInicioTarifa" onChange={alCambiar}/>
               </Form.Group>
+            </Row>   
 
-             
-            </Row>
-
-           
           </Form>
+
         </Modal.Body>
         <Modal.Footer>
           <Button variant="danger" onClick={handleClose}>
